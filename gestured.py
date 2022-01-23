9149d63e5806bf06ffa121b3e5a616626dc9a0a1
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-23 10:59:57 +0530

    base 3 done

--- gestured.py
+++ gestured.py
@@ -12,7 +12,54 @@ def capture(baseline_image):
      cv2.destroyAllWindows()
 def left(frame):
     left = cv2.imread("./gestured_image/Left.png")
-
+    delta=cv2.absdiff(frame,left)
+    threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+    (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            mouse.drag(100, 100, 0, 0, absolute=False, duration=0.1)
+            continue
+        continue
+def right(frame):
+    left = cv2.imread("./gestured_image/Right.png")
+    delta=cv2.absdiff(frame,left)
+    threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+    (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            mouse.drag(100, 100, 0, 0, absolute=False, duration=0.1)
+            continue
+        continue
+def up(frame):
+    left = cv2.imread("./gestured_image/Up.png")
+    delta=cv2.absdiff(frame,left)
+    threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+    (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            mouse.drag(100, 100, 0, 0, absolute=False, duration=0.1)
+            continue
+        continue
+def down(frame):
+    left = cv2.imread("./gestured_image/Down.png")
+    delta=cv2.absdiff(frame,left)
+    threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+    (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            mouse.drag(100, 100, 0, 0, absolute=False, duration=0.1)
+            continue
+        continue
+def click(frame):
+    left = cv2.imread("./gestured_image/Down.png")
+    delta=cv2.absdiff(frame,left)
+    threshold=cv2.threshold(delta,35,255, cv2.THRESH_BINARY)[1]
+    (contours,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            mouse.click('left')
+            continue
+        continue
 while(q==1):
     net,first = videoCaptureObject.read()
     baseline_image=cv2.cvtColor(first,cv2.COLOR_BGR2GRAY)
