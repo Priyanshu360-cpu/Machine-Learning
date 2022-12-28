8604aaec9b28da9f4d077e682ef5c61e2193d378
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-12-29 02:01:20 +0530

    data_preprocessing

---
+++ data_preprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+dataset=pd.read_csv('Data.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+imputer=SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+ct=ColumnTransformer([('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+X=np.array(ct.fit_transform(X))
+le=LabelEncoder()
+Y=le.fit_transform(Y)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1)
+sc=StandardScaler()
+X_train[:,3:]=sc.fit_transform(X_train[:,3:])
+X_test[:,3:]=sc.transform(X_test[:,3:])
+print(X_train)
+print(X_test)
+print(Y_test)
