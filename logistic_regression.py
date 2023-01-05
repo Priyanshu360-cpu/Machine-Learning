4dde2eec51b8b8075d3acb1cb2ea3d1382e82ca4
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2023-01-05 16:12:06 +0530

    logistic_regression

---
+++ logistic_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=0)
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+from sklearn.linear_model import LogisticRegression
+regressor=LogisticRegression()
+regressor.fit(X_train,Y_train)
+print(regressor.predict(sc.transform([[30,87000]])))
+Y_pred=regressor.predict(X_test)
+print(np.concatenate((Y_pred.reshape(len(Y_pred),1),Y_test.reshape(len(Y_test),1)),1))
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm=confusion_matrix(Y_test,Y_pred)
+print(cm) #confuison matrix
