9f3facdfb5a9fafbdf41c0cc11bdc0959e275360
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2023-01-12 00:41:27 +0530

    updated

--- logistic_regression.py
+++ logistic_regression.py
@@ -21,3 +21,18 @@ cm=confusion_matrix(Y_test,Y_pred)
 print(cm) #confuison matrix
 ce=accuracy_score(Y_test,Y_pred)
 print(ce) #accuracy score
+from matplotlib.colors import ListedColormap
+X_set, Y_set = sc.inverse_transform(X_train), Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, regressor.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1], c = ListedColormap(('salmon', 'dodgerblue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
