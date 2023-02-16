fab97e8da49f5474953c3c767f37ccf30b3380f2
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2023-02-16 23:02:18 +0530

    added files

---
+++ naive-bayers.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=0)
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+from sklearn.naive_bayes import GaussianNB
+regressor = GaussianNB()
+regressor.fit(X_train, Y_train)
+print(regressor.predict(sc.transform([[30,87000]])))
+Y_pred=regressor.predict(X_test)
+print(np.concatenate((Y_pred.reshape(len(Y_pred),1),Y_test.reshape(len(Y_test),1)),1))
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm=confusion_matrix(Y_test,Y_pred)
+print(cm) #confuison matrix
+ce=accuracy_score(Y_test,Y_pred)
+print(ce) #accuracy score
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, regressor.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('salmon', 'dodgerblue'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
